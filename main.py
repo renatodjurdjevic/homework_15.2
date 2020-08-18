c5eac5556142128f42a4b5f1345e0771a23f65f2
renatodjurdjevic <renato.durdevic23@gmail.com>
2020-08-18 18:58:45 +0200

    added links

--- main.py
+++ main.py
@@ -14,5 +14,17 @@ def about():
 def portfolio():
     return render_template("portfolio.html")
 
+@app.route("/portfolio/frizerskisalon")
+def frizerskisalon():
+    return render_template("https://gist.github.com/renatodjurdjevic/1f9e8105d967c0c9cd9fb2fdcb4d3ecf")
+
+@app.route("/portfolio/boogle")
+def boogle():
+    return render_template("https://gist.github.com/renatodjurdjevic/9c175d448cab9867767574827c09d452")
+
+@app.route("/portfolio/fakebook")
+def fakebook():
+    return render_template("https://gist.github.com/renatodjurdjevic/0de667a00532ecb6704b6d8bd2b29309")
+
 if __name__ == "__main__":
     app.run()
